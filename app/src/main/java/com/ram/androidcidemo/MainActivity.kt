18ed65c154ce7cbd88d0047044deaf5160f4df15
Ramkumar-code <ramkumarperumal810@gmail.com>
2026-04-02 00:52:29 +0530

    Add CI/CD workflow-1

--- app/src/main/java/com/ram/androidcidemo/MainActivity.kt
+++ app/src/main/java/com/ram/androidcidemo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AndroidCIDemoTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android CI/CD DEMO",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
